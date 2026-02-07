ae9700232b83fe2764eae3a866f9b46b4fa1e975
DavidHamon <davidhamon3@gmail.com>
2026-02-07 12:27:41 +0100

    feat : FSM for gamemanagement

---
+++ Assets/David's Folder/Scripts/GameManagement/States/MainMenuState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+[System.Serializable]
+public class MainMenuState : AGameState
+{
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+}
