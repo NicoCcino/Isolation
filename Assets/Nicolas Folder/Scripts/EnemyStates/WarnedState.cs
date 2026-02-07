df2b2e7241cde54105d53e3fc22091fd3e415629
NicoCcino <frangins_freeman@hotmail.fr>
2026-02-08 00:08:51 +0100

    Add animated exclamation mark to enemy in warned state

--- Assets/Nicolas Folder/Scripts/EnemyStates/WarnedState.cs
+++ Assets/Nicolas Folder/Scripts/EnemyStates/WarnedState.cs
@@ -5,10 +5,12 @@ public class WarnedState : AEnemyState
 {
     protected float warnedTimer = 0f;
     public float warnedTime = 2f;
+    public GameObject exclamationMarkGO;
 
-    public WarnedState(EnemyController enemyController, EnemyVision enemyVision, EnemyData enemyData, EnemyStateManager enemyStateManager, float warnedTime) : base(enemyController, enemyVision, enemyData, enemyStateManager)
+    public WarnedState(EnemyController enemyController, EnemyVision enemyVision, EnemyData enemyData, EnemyStateManager enemyStateManager, float warnedTime, GameObject exclamationMarkGO) : base(enemyController, enemyVision, enemyData, enemyStateManager)
     {
         this.warnedTime = warnedTime;
+        this.exclamationMarkGO = exclamationMarkGO;
     }
 
     public override void Enter()
@@ -17,10 +19,15 @@ public class WarnedState : AEnemyState
         enemyController.agent.speed = 0f;
         warnedTimer = 0f;
         // TO DO : Add jump animation and exclamation mark
+        // Display exclamation mark
+        exclamationMarkGO.SetActive(true);
+
     }
 
     public override void Exit()
     {
+        // Hide exclamation mark
+        exclamationMarkGO.SetActive(false);
     }
 
     public override void Update()
