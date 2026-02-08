9d493381d84e0ce462317ec4de22bc24a4bd86e5
Barbarpapa <maxaupetit@hotmail.fr>
2026-02-08 13:42:30 +0100

    [LD] Import Assets Package and Update LD with a bit of Propsing

--- Assets/Maxime's Folder/TimedEventManager.cs
+++ Assets/Maxime's Folder/TimedEventManager.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class TimedEventManager : Singleton<TimedEventManager>
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private TimedEvent[] TimedEventList;
-    [SerializeField] private AudioLoopBlender audioLoopBlender;
-    private Dictionary<TimedEvent,Boolean> TimedEventPlayMap; 
+    [SerializeField] private AudioSource audioSource;
+    private Dictionary<TimedEvent,bool> TimedEventPlayMap; 
     void Start()
     {
         foreach (TimedEvent timedEvent in TimedEventList)
         {
             TimedEventPlayMap.Add(timedEvent,false);
         }
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,8 +28,8 @@ public class TimedEventManager : Singleton<TimedEventManager>
                 && GameOutcomeManager.Instance.GetTimerProgression() >= timedEvent.RunDurationPercentageStart 
                 && GameOutcomeManager.Instance.GetTimerProgression()<= timedEvent.RunDurationPercentageStop)
             {
-                
-                    audioLoopBlender.PlayOverrideEvent(timedEvent.audioClip);
+                    audioSource.clip = timedEvent.audioClip;
+                    audioSource.Play();
                     TimedEventPlayMap[timedEvent]=true; 
                              
             }
