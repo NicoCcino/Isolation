a5ea5e072d30472ce397ddb34f369093a6ad75a3
NicoCcino <frangins_freeman@hotmail.fr>
2026-02-08 19:24:50 +0100

    Improve player anim (progressive speed and manage backwards speed)

--- Assets/Nicolas Folder/Scripts/PlayerAnimationController.cs
+++ Assets/Nicolas Folder/Scripts/PlayerAnimationController.cs
@@ -4,21 +4,37 @@ public class PlayerAnimationController : MonoBehaviour
 {
 
     private float speed;
+    private bool goingForward;
     public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         speed = Player.Instance.KinematicCarController.Motor.BaseVelocity.magnitude;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGoingForward()){
         speed = Player.Instance.KinematicCarController.Motor.BaseVelocity.magnitude;
+        }
+        else{
+        speed = - Player.Instance.KinematicCarController.Motor.BaseVelocity.magnitude;
+        }
+
 
         // Paramètres Animator
         animator.SetFloat("Speed", speed);
         //animator.SetBool("IsMoving", speed > 0.1f);
+    }
+
+    private bool isGoingForward()
+    {
+        float prod = Vector3.Dot(Player.Instance.KinematicCarController.Motor.BaseVelocity,-Player.Instance.transform.forward);
+        if (prod <= 0)
+        {
+            return false;
+        }
+            return true;
     }
 }
